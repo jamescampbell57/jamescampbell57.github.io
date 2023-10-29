280423e4cab2c1f6cdb2a1af3b55913703cbe4a2
James Campbell <jgc239@cornell.edu>
2023-10-29 05:39:35 -0400

    Building website

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all "Read More" buttons
+    const readMoreBtns = document.querySelectorAll('.read-more-btn');
+    
+    readMoreBtns.forEach((btn) => {
+      btn.addEventListener('click', function() {
+        const longText = this.previousElementSibling; // .long
+        const shortText = longText.previousElementSibling; // .short
+        
+        // Toggle text visibility
+        longText.style.display = (longText.style.display === 'none' || longText.style.display === '') ? 'block' : 'none';
+        shortText.style.display = (shortText.style.display === 'none' || shortText.style.display === '') ? 'block' : 'none';
+        
+        // Change button text
+        this.textContent = (this.textContent === 'Read More') ? 'Read Less' : 'Read More';
+      });
+    });
+  });
+  
